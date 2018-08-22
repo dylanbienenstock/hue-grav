7dff42463557a237b17f03b652e634ee40d043e9
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-08-22 14:03:14 -0700

    Add ng2-slide-down to dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Ng2SlideDownModule } from "ng2-slide-down";
+
 import { AppComponent } from './app.component';
 import { SimulationComponent } from './simulation/simulation.component';
 import { InterfaceComponent } from './interface/interface.component';
@@ -13,7 +15,8 @@ import { FullscreenService } from './fullscreen.service';
         InterfaceComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        Ng2SlideDownModule
     ],
     providers: [
         FullscreenService
