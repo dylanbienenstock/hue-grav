ac45d0731f29ffec5268e8daa34af9c461571cdb
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-08-25 12:21:40 -0700

    Make settings work (!!!)

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -17,12 +17,13 @@ export class SettingsComponent implements OnInit, AfterViewInit {
     public initialized: boolean = false;
 
     public booleanSettings: BooleanSetting[];
-    
     public numberSettings: NumberSetting[];
 
     public percentage: { [key: string]: number } = {};
     public leadingZeros: { [key: string]: string } = {};
     
+    public selectedBarContainer: HTMLElement; 
+
     ngOnInit() {
         this.booleanSettings = [
             {
@@ -31,10 +32,6 @@ export class SettingsComponent implements OnInit, AfterViewInit {
                 default: true
             }
         ];
-
-        // make config object flat then
-        // give property name as string instead
-        // of model
 
         this.numberSettings = [
             {
@@ -48,7 +45,7 @@ export class SettingsComponent implements OnInit, AfterViewInit {
                 name: "Velocity Dampening",
                 binding: "velocityDampening",
                 min: 0.0025,
-                max: 0.02,
+                max: 0.035,
                 default: 0.0075
             },
             {
@@ -60,24 +57,24 @@ export class SettingsComponent implements OnInit, AfterViewInit {
             },
             {
                 name: "Particle Count",
-                binding: "particles.count",
+                binding: "particleCount",
                 min: 1,
                 max: 2048,
                 default: 1024
             },
             {
                 name: "Particle Size",
-                binding: "particles.size",
+                binding: "particleSize",
                 min: 1,
                 max: 16,
                 default: 6 
             },
             {
                 name: "Particle Opacity",
-                binding: "particles.opacity",
-                min: 1,
-                max: 2048,
-                default: 1024
+                binding: "particleOpacity",
+                min: 0,
+                max: 1,
+                default: 0.75
             },
             {
                 name: "Explosion Trigger Radius",
@@ -105,19 +102,20 @@ export class SettingsComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
         for (let setting of this.numberSettings) {
+            setting.value = setting.default;
+            
             this.percentage[setting.name] = this.toPercent(setting);
             this.leadingZeros[setting.name] = this.getLeadingZeros(setting);
         }
 
         setTimeout(() => {
-
             this.initialized = true;
         });
     }
 
     toPercent(setting: NumberSetting): number {
         let range = setting.max - setting.min;
-        let ratio = 1 - (setting.max - setting.default) / range;
+        let ratio = 1 - (setting.max - setting.value) / range;
         let percent = ratio * 100;
         let percentRounded = Math.round(percent);
 
@@ -132,5 +130,73 @@ export class SettingsComponent implements OnInit, AfterViewInit {
         }
 
         return "";
+    }
+
+    // Thanks! https://stackoverflow.com/users/3183756/caub
+    getSignificantDigits(n) {
+        var x = Math.abs(n);
+        if (x===0) return 0;
+        var p = Math.floor(Math.log10(x));
+        if (p>0) x = x / (10**p); 
+        else if (p<0) x = x * (10**-p);
+        return (x+'').length-1||1;
+    }
+
+    mouseXToRatio(barContainer: HTMLElement, mouseX: number) {
+        let clientRect = barContainer.getBoundingClientRect();
+        let relativeX = mouseX - clientRect.left;
+        let ratio = relativeX / clientRect.width;
+        let ratioConstrained = Math.max(0, Math.min(1, ratio));
+
+        return ratioConstrained;
+    }
+
+    onMouseDownBarContainer(barContainer: HTMLElement, mouseX: number) {
+        this.selectedBarContainer = barContainer;
+        this.adjustNumberSetting(mouseX);        
+    }
+
+    adjustNumberSetting(mouseX: number) {
+        if (this.selectedBarContainer) {
+            let settingName = 
+                this.selectedBarContainer.getAttribute("data-setting-name");
+
+            let ratio = 
+                this.mouseXToRatio(this.selectedBarContainer, mouseX);
+                
+            let setting = 
+                this.numberSettings.find(setting => setting.name == settingName);
+
+            let range = setting.max - setting.min;
+
+            let value = setting.min + range * ratio;
+
+            let significantDigits = 
+                this.getSignificantDigits(setting.default);
+
+            let valueTruncatedString = value.toPrecision(significantDigits);
+
+            let valueTruncated = parseFloat(valueTruncatedString);
+
+            let newConfig = Object.assign({}, this.config);
+            
+            newConfig[setting.binding] = valueTruncated;
+            setting.value = valueTruncated;
+
+            this.configChanged.emit(newConfig);
+
+            this.percentage[settingName] = Math.round(ratio * 100);
+            this.leadingZeros[settingName] = this.getLeadingZeros(setting);
+        }
+    }
+ 
+    @HostListener("window:mouseup")
+    onMouseUp() {
+        this.selectedBarContainer = null;
+    }
+
+    @HostListener("window:mousemove", ["$event"])
+    onMouseMove(e: MouseEvent) {
+        this.adjustNumberSetting(e.clientX);
     }
 }
